968f36f8517e7f957d230434f66d212499036ce0
simrat <simratb94@gmail.com>
2023-04-05 15:00:00 -0700

    thesaurus project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { React, useState } from "react";
+import Axios from "axios";
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
+ 
 function App() {
+  // Setting up the initial states using react hook 'useState'
+ 
+  const [data, setData] = useState();
+  const [searchWord, setSearchWord] = useState("");
+ 
+  // Function to fetch information on button
+  // click, and set the data accordingly
+  function getMeaning() {
+    Axios.get(
+      `https://api.datamuse.com/words?ml=${searchWord}&max=4`
+    ).then((response) => {
+      console.log(data);
+      setData(response.data);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Find a similar word</h1>
+      <div className="searchBox">
+ 
+        
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => {
+            setSearchWord(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            getMeaning();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <FaSearch size="20px" />
+        </button>
+      </div>
+      {data && (
+        <div className="showResults">
+          <h2>
+            <div>
+            {data[0].word}{" "} 
+            </div>
+            <div>
+            {data[1].word}{" "}
+            </div>
+            <div>
+            {data[2].word}{" "}
+            </div>
+            {data[3].word}{" "}
+            </h2>
+ 
+        </div>
+      )}
     </div>
   );
 }
-
+ 
 export default App;
